409c61f3775363fdace5657d3875e254b52471da
fommarddavid <david.fommard@gmail.com>
2020-08-04 08:41:39 +0200

    setup routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routes.auth);
 
 app.listen(process.env.PORT, () => (
   console.log(`App listening on port ${process.env.PORT} !`)
