c4f2dcec2f2b74c855505a67b3dc4d494953c554
fommarddavid <david.fommard@gmail.com>
2020-08-05 16:46:50 +0200

    frontend: model install

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+// == Import : npm
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Reset } from 'styled-reset';
+
+// == Import : local
+import App from 'src/components/App';
+import store from 'src/store';
+
+// == Render
+// 1. Élément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM virtuel)
+const rootReactElement = (
+  <Provider store={store}>
+    <Reset />
+    <App />
+  </Provider>
+);
+
+render(rootReactElement, document.getElementById('root'));
