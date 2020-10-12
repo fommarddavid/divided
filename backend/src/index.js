356863344c44aa680f2a89f071efd4e5e32fc8f7
fommarddavid <david.fommard@gmail.com>
2020-10-12 16:26:48 +0200

    test prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
+import path from 'path';
 
 import routes from './routes';
 
@@ -15,6 +16,7 @@ app.use(compression());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/api', routes.auth);
 app.use('/api/password', routes.password);
